e1928ef7379b3c1359f98ceb8e5c8cdb449a399b
g8rmba <richard.foster@ge.com>
2015-01-23 20:00:52 -0500

    Assignment Complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  # function to set up a static local variable for an inverse matrix
+  # calling method <var> <- makeCacheMatrix(<invertableMatrix>)
+  #                <var>$get(), <var>$set(),<var>$getmatrix(), <var>$setmatrix()
+  # variables x = original matrix, m = inverted matrix
+  m <- NULL
+  #function to store the original matrix
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  # funtion to return the original matrix
+  get <- function() x
+  # function to store the inverted matrix in the local variable
+  setmatrix <- function(solve) m <<- solve
+  #function to return the inverse matrix
+  getmatrix <- function() m
+  #create a list of function names
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # function to return an inverse matrix
+  # the first time the function is called the inverse matrix is solved and cached
+  # subsequent calls to the return the cached values
+  # calling method <var> <- cacheSolve(<var> as makeCacheMatrix)
+  # variables m = static results, data = matrix to be inverted, x = makeCacheMatrix
+  m <- x$getmatrix()
+  # check to see if first time called and no cached results
+  if(!is.null(m)) {
+    message("getting cached data")
+    # return the cahced inverse matrix
+    return(m)
+  }
+  # no prior results so get matrix to be inverted
+  data <- x$get()
+  # invert the matrix
+  m <- solve(data, ...)
+  #store the matrix in the cache location
+  x$setmatrix(m)
+  # return the inverse matrix
+  m
 }
